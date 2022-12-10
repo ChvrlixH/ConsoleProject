7a781b3f454e209204f56413cc900eb0a3839391
ChvrlixH <ehedzade.huseyn@gmail.com>
2022-12-10 05:04:16 +0400

    Checking

---
+++ Entities/Bank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_application.Entities
+{
+    internal class Bank
+    {
+        public int Id;
+        public User? Users;
+    }
+}
---
+++ Service/BankService.cs
@@ -0,0 +1,14 @@
+﻿using Bank_application.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_application.Service
+{
+    internal interface BankService
+    {
+        
+    }
+}
